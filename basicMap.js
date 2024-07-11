7da9cccd2ff135fba2a2123607d64a9478dbe073
bengassler <ffireworks843@gmail.com>
2024-07-11 14:02:12 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,1 +1,13 @@
 console.log("js works");
+
+//35.217665 N, 80.831473 W -> 35.217665 , -80.831473
+
+let map = L.map("map", {
+center: [35.217665 , -80.831473],
+  zoom: 8
+});
+
+//import leaflet quickstart template
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);

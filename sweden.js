5103c33c3945dd7e76fd9c9257e66ac5094b9510
bengassler <ffireworks843@gmail.com>
2024-07-16 13:32:51 -0400

    Update sweden.js

--- sweden.js
+++ sweden.js
@@ -3,7 +3,7 @@ console.log("js works");
 //35.217665 N, 80.831473 W -> 35.217665 , -80.831473
 
 let map = L.map("map", {
-center: [35.217665 , 18.643501],
+center: [59.32938 , 18.643501],
   zoom: 15
 });
 
@@ -13,7 +13,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let marker = L.marker(
-  [35.217665 , 18.643501], {
+  [59.32938 , 18.643501], {
     title: "Sweden"
   }
 ).bindPopup(`<h2>Hello from Sweden!</h2><hr><iframe width="280" height="157" src="https://www.youtube.com/embed/6zEIvZqs0-Y?si=HOsw05DyyuFa2DVm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`)
